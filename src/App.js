4ac89a157a459f061a6c77f7a07f9774098b7097
Monica Powell <monica@jopwell.com>
2017-05-17 19:01:52 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+/// importing the library React and one of its classes Component
+
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <h1>Hello, world.</h1>
+    );
+  }
+}
